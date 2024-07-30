a26ae6fbe3840f4bc42049b9b355b1e876025a0c
shristipandey15 <shristipandey73@gmail.com>
2024-07-30 15:29:41 +0545

    Initialize client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navigation from './customer/components/Navigation/Navigation.js'
+import Homepage from './customer/pages/HomePage/Homepage.jsx'
+
+function App() {
+  return (
+    <div className="">
+        <Navigation/>
+     <div>
+        <Homepage/>
+     </div>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/customer/pages/HomePage/Homepage.jsx
@@ -0,0 +1,16 @@
+// customer/components/Homepage.js
+import React from 'react';
+import MainCarosel from '../../components/HomeCarosel/MainCarosel';
+
+function Homepage() {
+  return (
+    <div>
+      <MainCarosel/>
+      <div>
+        Other section
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
