65496425d0eedeacb2f786c7066105f3674173c9
anisha stha <stha.anisha16@gmail.com>
2024-08-07 18:31:21 +0545

    feat:services

--- e-commerce-api/src/services/cartItem.service.js
+++ e-commerce-api/src/services/cartItem.service.js
@@ -1,4 +1,6 @@
+const CartItem = require("../models/cartItem.model.js");
 const userService = require("../services/user.service.js");
+
 async function updateCartItem(userId, cartItemId, cartItemData) {
     try {
         const item = await findCartItemById(cartItemId,);
@@ -9,7 +11,46 @@ async function updateCartItem(userId, cartItemId, cartItemData) {
         if (!user) {
             throw new Error("user not found :", userId);
         }
+        if (user._id.toString() === userId.toString()) {
+            item.quality = cartItemData.quality;
+            item.price = item.quantity * item.product.price;
+            item.discountedPrice = item.quality * item.product.discountedPrice;
+            const updateCartItem = await item.save();
+            return updateCartItem;
+        }
+            else {
+                throw new Error("you cant update this cart item");
+
+            }
+
+
+            
+        
     } catch (error) {
+        throw new Error(error.message);
         
     }
+}
+async function removeCartItem(userId, cartItemId) {
+    const cartItem = await findCartItemById(cartItemId);
+    const user = await userService.findUserById(userId);
+
+    if (user._id.toString() === cartItem.userId.toString()) {
+        await CartItem.findByIdAndDelete(cartItemId);
+
+    }
+    throw new Error("you cant remove another user's item");
+}
+async function findCartItemById(cartItemId) {
+    const cartItem = await findCartItemById(cartItemId);
+    if (cartItem) {
+        return cartItem;
+    } else {
+        throw new Error("cartitem not found with id " + cartItemId);
+    }
+}
+module.exports = {
+    updateCartItem,
+    removeCartItem,
+    findCartItemById
 }
